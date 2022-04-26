2b10a34554c787210a85d834200804c5dda2ecfd
Andreea B <burada.andreea@outlook.com>
2022-04-26 20:01:51 +0300

    Added local files

---
+++ seminars/seminar_01/seminar_01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System; //referenced namespace
+
+namespace NameSpaceProgram
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Console.WriteLine(args.Length);
+			foreach (string arg in args)
+			{
+				Console.WriteLine("Argument: {0}", arg);
+			}
+
+		} //end main
+	}//end class
+} //end namespace
---
+++ seminars/seminar_02/seminar_02/Program.cs
@@ -0,0 +1,68 @@
+﻿using System; //referenced namespace
+using System.Text;
+
+//namespace NameSpaceProgram
+//{
+//	internal class Program
+//	{
+//		public static int Test (int x)
+//        {
+//			return x + 10;
+//        } 
+//		private static void Main(string[] args)
+//		{
+//			/*bool boolean = true;
+//			byte b = 128; // unsigned: 0 to 255
+//			short s = 1000; // -32,768 to 32,767
+//			int i = 12000; // signed 32 bit integer
+//			int maxInt = int.MaxValue;
+//			long l = 100000000; //signed 64 bit integer
+//			long maxLong = long.MaxValue;
+//			char c = '\n'; //UTF-8 | 16 bit character
+//			float f = 0.00221f; //32 bit float
+//			double d = 0.012221223; // 64 bit float
+//			decimal e = 0.2m; // 96 bit number
+//			string str = "Hello World";*/
+
+//			//Console.WriteLine($"{boolean} {b} {s} {i} {maxInt} {l} {maxLong} {c} {f} {d} {e} {str}");
+
+//			DateTime date = DateTime.Now;
+//			Console.WriteLine("Hello! What is your name?");
+//			string name = Console.ReadLine();
+//			Console.WriteLine($"\nYour name is: {name}\nToday's date: {date.ToString("dd MMMM yyyy H:mm")}\nHello!");
+
+//		}
+//	}
+//} 
+
+
+namespace NameSpaceProgram
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            //string str = "Test ";
+            //str += "Text ";
+            //str += " 123";
+
+            //// StringBuilder
+
+            //StringBuilder sb = new StringBuilder();
+
+            //sb.Append("Test ");
+            //sb.Append("Text ");
+            //sb.Append(" 123");
+
+            //Console.WriteLine(str);
+
+            //string a = "Hello world";
+            //string b = "Hello world";
+
+            //Console.WriteLine(a.Equals(b));
+
+            //------------Dynamic Types-------------
+
+        }
+    }
+}
