5b6a1bb64e139659a3888b4fff82154cda5f8b37
Andreea B <burada.andreea@outlook.com>
2022-05-05 18:37:58 +0300

    Added 05 & 06

---
+++ github_self_study/06_winForms_listView/ListViewSample/MainForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using ListViewSample.Entities;
+
+namespace ListViewSample
+{
+	internal partial class MainForm : Form
+	{
+		#region Attributes
+		private readonly List<Participant> _participants;
+		#endregion
+
+		public MainForm()
+		{
+			InitializeComponent();
+
+			_participants = new List<Participant>();
+		}
+
+		#region Methods
+		public void DisplayParticipants()
+		{
+			lvParticipants.Items.Clear();
+
+			foreach (Participant participant in _participants)
+			{
+				var listViewItem = new ListViewItem(participant.LastName);
+				listViewItem.SubItems.Add(participant.FirstName);
+				listViewItem.SubItems.Add(participant.BirthDate.ToShortDateString());
+
+				if (participant.BirthDate < new DateTime(DateTime.Now.Year - 18, DateTime.Now.Month, DateTime.Now.Day) )
+				{
+					listViewItem.ImageKey = "adult.png";
+					listViewItem.Group = lvParticipants.Groups["adults"];
+				}
+				else
+				{
+					listViewItem.ImageKey = "child.png";
+					listViewItem.Group = lvParticipants.Groups["children"];
+				}
+
+				lvParticipants.Items.Add(listViewItem);
+			}
+		}
+		#endregion
+
+		#region Events
+
+		private void btnAdd_Click(object sender, EventArgs e)
+		{
+			string firstName = tbFirstName.Text;
+			string lastName = tbLastName.Text;
+			DateTime birthDate = dtpBirthDate.Value;
+
+			var participant = new Participant(lastName, firstName, birthDate);
+			_participants.Add(participant);
+
+            #region ClearFields
+            tbFirstName.Clear();
+			tbLastName.Clear();
+			dtpBirthDate.Value = DateTime.Now;
+            #endregion
+
+            DisplayParticipants();
+		}
+		private void btnDetails_Click(object sender, EventArgs e)
+		{
+			lvParticipants.View = View.Details;
+		}
+
+		private void btnList_Click(object sender, EventArgs e)
+		{
+			lvParticipants.View = View.List;
+		}
+		private void btnLargeIcon_Click(object sender, EventArgs e)
+		{
+			lvParticipants.View = View.LargeIcon;
+		}
+		private void btnSmallIcon_Click(object sender, EventArgs e)
+		{
+			lvParticipants.View = View.SmallIcon;
+		}
+
+		private void btnTiles_Click(object sender, EventArgs e)
+		{
+			lvParticipants.View = View.Tile;
+		}
+		#endregion
+	}
+}
---
+++ github_self_study/06_winForms_validation/ValidationCustomExceptions/MainForm.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using ValidationCustomExceptions.Entities;
+using ValidationExceptionHandeling.Entities;
+
+namespace ValidationCustomExceptions
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		#region Events
+		private void tbLastName_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			if (!IsLastNameValid())
+			{
+				e.Cancel = true; //prevents the user from changing the focus to another control
+				errorProvider.SetError((Control)sender, "\"Last name\" cannot be empty.");
+			}
+		}
+
+		private void tbLastName_Validated(object sender, EventArgs e)
+		{
+			errorProvider.SetError((Control)sender, string.Empty);
+		}
+
+		private void tbFirstName_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			if (!IsFirstNameValid())
+			{
+				e.Cancel = true; //prevents the user from changing the focus to another control
+
+				errorProvider.SetError((Control) sender, "First Name is empty!");
+			}
+		}
+		
+		private void tbFirstName_Validated(object sender, EventArgs e)
+		{
+			//https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.errorprovider.clear
+			errorProvider.SetError((Control)sender, string.Empty);
+		}
+
+		private void btnAdd_Click(object sender, EventArgs e)
+		{
+
+			//https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.containercontrol.validatechildren
+			if (!ValidateChildren())
+			{
+				//An ErrorProvider control should
+				MessageBox.Show("The form contains errors. Please check the input data",
+					"ERROR: 404",
+					MessageBoxButtons.OK,
+					MessageBoxIcon.Warning);
+
+				return;
+			}
+
+			try
+			{
+				string firstName = tbFirstName.Text.Trim();
+				string lastName = tbLastName.Text.Trim();
+				DateTime birthDate = dtpBirthDate.Value;
+				string ssn = tbSSN.Text.Trim();
+				GenderEnum gender = GenderEnum.Male;
+				if (rbFemale.Checked)
+					gender = GenderEnum.Female;
+				var participant = new Participant(lastName, firstName, birthDate, gender, ssn);
+				//TODO Logic for adding the participant to the ListView
+			}
+			catch (InvalidBirthDateException ex)
+			{
+				//Expected exception
+				MessageBox.Show(string.Format("The birth date {0}.{1}.{2} is invalid", ex.BirthDate.Year, ex.BirthDate.Month, ex.BirthDate.Day));
+			}
+			catch (Exception)
+			{
+				//UnExpected exception
+				MessageBox.Show("An exception has been encountered! Please contact the technical support.");
+
+				//Log the exception using:
+				// - Log4Net
+				// - Application Insights
+			}
+			finally
+			{
+				Debug.WriteLine("Always executed");
+			}
+		}
+		#endregion
+
+		#region Methods
+		private bool IsLastNameValid()
+		{
+			return !string.IsNullOrWhiteSpace(tbLastName.Text.Trim());
+		}
+
+		private bool IsFirstNameValid()
+		{
+			return !string.IsNullOrWhiteSpace(tbFirstName.Text.Trim());
+		}
+		#endregion
+	}
+}
